b096b5fb883c6939800cf73ec34b4e48d3f0d415
abdullahsoomro-blocship <abdullah.soomro@blocship.io>
2024-05-28 11:34:10 +0500

    01_output: Learning about printing, runtime types, some data types and syntax

---
+++ 01_output.php
@@ -0,0 +1,24 @@
+
+<?php
+    // echo 'Hello World';
+    // echo 12, 'hello', 12.1;
+    
+    // print 2;
+
+    // print_r([1,2,3]);
+
+    // var_dump('asda');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?= 'Post One'; ?></h1>
+    
+</body>
+</html>
+
